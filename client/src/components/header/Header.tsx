2d8c8fbbd506af1c12942a134c9cd95ec9b61705
Huy <huynd.nguyen@gmail.com>
2020-12-22 01:48:24 +0700

    fix profile

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -34,85 +34,97 @@ class Header extends React.Component {
     const { menuMode, menuVisible, isLogin, profile } = this.state;
 
     const menu = (
-      <Menu mode={menuMode as any} id="nav" key="nav">
-        <Menu.Item key="home">
-          <Link to="/">Trang chủ</Link>
+      <Menu mode={menuMode as any} id='nav' key='nav'>
+        <Menu.Item key='home'>
+          <Link to='/'>Trang chủ</Link>
         </Menu.Item>
-        <Menu.Item key="accommod">
-          <Link to="/accommod">
+        <Menu.Item key='accommod'>
+          <Link to='/accommod'>
             <span>Tìm trọ</span>
           </Link>
         </Menu.Item>
-        <Menu.Item key="post">
-          <Link to="/post">
+        <Menu.Item key='post'>
+          <Link to='/post'>
             <span>Đăng bài</span>
           </Link>
         </Menu.Item>
-        <Menu.Item key="search">
-          <Link to="/search">
+        <Menu.Item key='search'>
+          <Link to='/search'>
             <span>Tìm kiếm</span>
           </Link>
+        </Menu.Item>
+        <Menu.Item>
+          {isLogin ? (
+            <div>
+              {
+                <img
+                  // src={(profile as any).imageUrl}
+                  src='https://vnu.edu.vn/upload/2014/11/17202/image/Logo-VNU-1995.jpg'
+                  alt='avatar'
+                  height={25}
+                  style={{ borderRadius: 20, marginRight: 20 }}
+                />
+              }
+              <Link to='/profile/user-info'>
+                <b className='mr-3'>{(profile as any).name}</b>
+              </Link>
+              <Button
+                onClick={logout}
+                className='button'
+                style={{ borderRadius: 32 }}
+              >
+                <span className='buttonText'>Đăng xuất</span>
+              </Button>
+            </div>
+          ) : (
+            <Button
+              className='buttonText'
+              onClick={() => {
+                window.location.href = '/login';
+              }}
+              style={{ borderRadius: 32 }}
+            >
+              Đăng nhập
+            </Button>
+          )}
         </Menu.Item>
       </Menu>
     );
 
     return (
       <>
-        <div id="header" className="header container">
+        <div id='header' className='header container'>
           {menuMode === 'inline' ? (
             <Popover
-              overlayClassName="popover-menu"
-              placement="bottomRight"
+              overlayClassName='popover-menu'
+              placement='bottomRight'
               content={menu}
-              trigger="click"
+              trigger='click'
               visible={menuVisible}
               arrowPointAtCenter
             >
-              <Icon className="nav-phone-icon" type="menu" />
+              <Icon
+                className='nav-phone-icon'
+                type='menu'
+                onClick={() => {
+                  this.setState({ menuVisible: !menuVisible });
+                }}
+              />
             </Popover>
           ) : null}
           <Row>
             <Col xxl={6} xl={6} lg={8} md={8} sm={24} xs={24}>
-              <Link to="/">
-                <div id="logo">
-                  <img src={logo} alt="logo" />
+              <Link to='/'>
+                <div id='logo'>
+                  <img src={logo} alt='logo' />
                   <b>EASY ACCOMMODATION</b>
                 </div>
               </Link>
             </Col>
             <Col xxl={18} xl={18} lg={16} md={16} sm={0} xs={0}>
-              <div className="header-meta">
-                <div id="preview">
-                  {isLogin ? (
-                    <>
-                      {
-                        <img
-                          // src={(profile as any).imageUrl}
-                          src="https://vnu.edu.vn/upload/2014/11/17202/image/Logo-VNU-1995.jpg"
-                          alt="avatar"
-                          height={25}
-                          style={{ borderRadius: 20, marginRight: 20 }}
-                        />
-                      }
-                      <Link to='/profile/user-info'>
-                        <b className="mr-3">{(profile as any).name}</b>
-                      </Link>
-                      <Button onClick={logout} className="button">
-                        <span className="buttonText">Đăng xuất</span>
-                      </Button>
-                    </>
-                  ) : (
-                    <Button
-                      className="buttonText"
-                      onClick={() => {
-                        window.location.href = '/login';
-                      }}
-                    >
-                      Đăng nhập
-                    </Button>
-                  )}
-                </div>
-                {menuMode === 'horizontal' ? <div id="menu">{menu}</div> : null}
+              <div className='header-meta'>
+                <div id='preview'></div>
+                {menuMode === 'horizontal' ? <div id='menu'>{menu}</div> : null}
               </div>
             </Col>
           </Row>
--- client/src/scenes/profile/ProfileRouter.tsx
+++ client/src/scenes/profile/ProfileRouter.tsx
@@ -1,43 +1,40 @@
-import { UserInfo } from "./UserInfo";
-import { ApprovingPost } from "./ApprovingPost";
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { ActivePost } from "./ActivePost";
+import { UserInfo } from './UserInfo';
+import { ApprovingPost } from './ApprovingPost';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { ActivePost } from './ActivePost';
 
 const routes = [
-    {
-        path: '/profile/user-info',
-        component: UserInfo,
-        private: true,
-    },
-    {
-        path: '/profile/approving-post',
-        component: ApprovingPost,
-        private: true,
-    },
-    {
-        path: '/profile/active-post',
-        component: ActivePost,
-        private: true,
-    },
+  {
+    path: '/profile/user-info',
+    component: UserInfo,
+  },
+  {
+    path: '/profile/approving-post',
+    component: ApprovingPost,
+  },
+  {
+    path: '/profile/active-post',
+    component: ActivePost,
+  },
 ];
 export default function UserProfile() {
-    return (
-            <Switch>
-                {routes.map((route, i) => (
-                    <RouteWithSubRoutes key={i} {...route} />
-                ))}
-            </Switch>
-    );
+  return (
+    <Switch>
+      {routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route} />
+      ))}
+    </Switch>
+  );
 }
 function RouteWithSubRoutes(route: any) {
-    return (
-        <Route
-            path={route.path}
-            render={props => (
-                // pass the sub-routes down to keep nesting
-                <route.component {...props} routes={route.routes} />
-            )}
-        />
-    );
+  return (
+    <Route
+      path={route.path}
+      render={(props) => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
 }
