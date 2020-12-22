f73a1483625dce0a8edbdacc3b2b61a693ede49a
Mạnh Đặng <57102439+manhdvuetk63@users.noreply.github.com>
2020-12-22 15:51:17 +0700

    fix bug

--- server/src/UET.EasyAccommod.Application/Sales/Dto/Output/ApartmentListDto.cs
+++ server/src/UET.EasyAccommod.Application/Sales/Dto/Output/ApartmentListDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Abp.Application.Services.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ using UET.EasyAccommod.MasterTable.UnitPrice.Dto;
 
 namespace UET.EasyAccommod.Sales.Dto.Output
 {
-    public class ApartmentListDto
+    public class ApartmentListDto : EntityDto<long>
     {
         public string Title { get; set; }
         public string Detail { get; set; }
