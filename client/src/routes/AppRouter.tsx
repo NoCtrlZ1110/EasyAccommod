8315e83fb1da0b3ecb15558b13bf8e229be9f507
Mạnh Đặng <57102439+manhdvuetk63@users.noreply.github.com>
2020-12-24 07:27:19 +0700

    fix conflic'

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -6,7 +6,6 @@ import Home from '../scenes/home/Home';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import {Login} from '../scenes/login/Login';
-import TEST from '../scenes/test';
 import AccommodList from '../scenes/accommod/AccommodList';
 import { SignUp } from '../scenes/sign_up/SignUp';
 import { SearchPage } from '../scenes/search/Search';
@@ -41,11 +40,6 @@ const routes = [
   {
     path: '/post',
     component: Post,
-    private: true,
-  },
-  {
-    path: '/test',
-    component: TEST,
     private: true,
   },
   {
